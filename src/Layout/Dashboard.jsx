39524aeca117763a291e03893001ff89bdc3e82f
afsana-jannat <afsanajannat205@gmail.com>
2023-11-28 08:49:35 +0600

    remove a small part

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -10,118 +10,118 @@ const Dashboard = () => {
     const [isAdmin] = useAdmin();
     return (
         <div className="flex">
-        {/* dashboard side bar */}
-        <div className="w-64 min-h-screen bg-red-300">
-            <ul className="menu p-4 font-bold text-white">
-                {
-                    isAdmin ? <>
+            {/* dashboard side bar */}
+            <div className="w-64 min-h-screen bg-red-300">
+                <ul className="menu p-4 font-bold text-white">
+                    {
+                        isAdmin ? <>
                             <li>
-                    <NavLink to="/dashboard/adminprofile">
-                        <FaHome></FaHome>
-                       Admin Profile</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/addMeals">
-                        <FaUtensils></FaUtensils>
-                        Add Meals</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/updatemeals">
-                        <FaBook></FaBook>
-                       Update Meals</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/users">
-                        <FaUser></FaUser>
-                        Manage Users</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/cart">
-                        <FaShoppingCart></FaShoppingCart>
-                        My Cart</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/allmeals">
-                        <FaList></FaList>
-                        All Meals</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/review">
-                        <FaAd></FaAd>
-                        Review</NavLink>
-                </li>
-                    </>
-                    :
-                    <>
+                                <NavLink to="/dashboard/adminprofile">
+                                    <FaHome></FaHome>
+                                    Admin Profile</NavLink>
+                            </li>
                             <li>
-                    <NavLink to="/dashboard/adminHome">
-                        <FaHome></FaHome>
-                       Admin Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/manageItems">
-                        <FaList></FaList>
-                        Manage Items </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/bookings">
-                        <FaBook></FaBook>
-                        Manage Bookings</NavLink>
-                </li>
-                {/* <li>
+                                <NavLink to="/dashboard/addMeals">
+                                    <FaUtensils></FaUtensils>
+                                    Add Meals</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/updatemeals">
+                                    <FaBook></FaBook>
+                                    Update Meals</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/users">
+                                    <FaUser></FaUser>
+                                    Manage Users</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/cart">
+                                    <FaShoppingCart></FaShoppingCart>
+                                    My Cart</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/allmeals">
+                                    <FaList></FaList>
+                                    All Meals</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/review">
+                                    <FaAd></FaAd>
+                                    Review</NavLink>
+                            </li>
+                        </>
+                            :
+                            <>
+                                <li>
+                                    <NavLink to="/dashboard/adminHome">
+                                        <FaHome></FaHome>
+                                        Admin Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/manageItems">
+                                        <FaList></FaList>
+                                        Manage Items </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/bookings">
+                                        <FaBook></FaBook>
+                                        Manage Bookings</NavLink>
+                                </li>
+                                {/* <li>
                     <NavLink to="/dashboard/users">
                         <FaUser></FaUser>
                         All Users</NavLink>
                 </li> */}
-                   
-                    <li>
-                    <NavLink to="/dashboard/useHome">
-                        <FaHome></FaHome>
-                        User Home</NavLink>
-                </li>
-                <li>
+
+                                <li>
+                                    <NavLink to="/dashboard/useHome">
+                                        <FaHome></FaHome>
+                                        User Home</NavLink>
+                                </li>
+                                {/* <li>
                     <NavLink to="/dashboard/reservation">
                         <FaCalendar></FaCalendar>
                         Reservation</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard/cart">
-                        <FaShoppingCart></FaShoppingCart>
-                        My Cart ({cart.length}) </NavLink>
-                </li>
-                
-                <li>
-                    <NavLink to="/dashboard/review">
-                        <FaAd></FaAd>
-                        Review</NavLink>
-                </li>
-              
-                    </>
-                }
-                
-                {/* shared nav links */}
-                <div className="divider"></div>
-                <li>
-                    <NavLink to="/">
-                        <FaHome></FaHome>
-                        Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/order/salad">
-                        <FaBook></FaBook>
-                        Meals</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/order/contact">
-                        <FaEnvelope></FaEnvelope>
-                        Contact</NavLink>
-                </li>
-            </ul>
+                </li> */}
+                                <li>
+                                    <NavLink to="/dashboard/cart">
+                                        <FaShoppingCart></FaShoppingCart>
+                                        My Cart ({cart.length}) </NavLink>
+                                </li>
+
+                                <li>
+                                    <NavLink to="/dashboard/review">
+                                        <FaAd></FaAd>
+                                        Review</NavLink>
+                                </li>
+
+                            </>
+                    }
+
+                    {/* shared nav links */}
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to="/">
+                            <FaHome></FaHome>
+                            Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/salad">
+                            <FaBook></FaBook>
+                            Meals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/contact">
+                            <FaEnvelope></FaEnvelope>
+                            Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex-1 p-8">
+                <Outlet></Outlet>
+            </div>
         </div>
-        <div className="flex-1 p-8">
-            <Outlet></Outlet>
-        </div>
-    </div>
     );
 };
 
